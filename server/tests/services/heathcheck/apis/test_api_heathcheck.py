a2d2ad6dece344c3566a36b0380fd4e0f10bf1b2
Enrico Goerlitz <private.email@gmail.com>
2024-01-29 21:24:13 +0100

    u

--- server/tests/services/heathcheck/apis/test_api_heathcheck.py
+++ server/tests/services/heathcheck/apis/test_api_heathcheck.py
@@ -1,6 +1,16 @@
+import json
 from flask.testing import FlaskClient
 
 
 def test_heathcheck(client: FlaskClient):
+    # given
+
+    # when
     response = client.get("/api/v1/heathcheck")
-    assert response.status_code == 200
+
+    status_code = response.status_code
+    res_data = json.loads(response.data.decode("utf-8"))
+
+    # then
+    assert status_code == 200
+    assert res_data == {"heathcheck": "ok"}
--- server/tests/services/recipe/apis/test_api_category.py
+++ server/tests/services/recipe/apis/test_api_category.py
@@ -2,4 +2,9 @@ from flask import Flask, testing
 
 
 def test_category_get(app: Flask, client: testing.FlaskClient):
+    # given
+
+    # when
+
+    # then
     pass
